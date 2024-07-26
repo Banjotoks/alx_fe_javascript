b880eb1c419c7873dc1c4a6dc6426d9fc6d00bda
Banjo Adetokunbo <banjotokunboh@gmail.com>
2024-07-26 08:07:07 +0100

    Building a Dynamic Content Generator with Advanced DOM Manipulation

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -28,11 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.log('Failed to add quote: Missing text or category');
         alert('Please enter both a quote and a category.');
-      }
-    }
+      } }
     
     newQuoteButton.addEventListener('click', displayRandomQuote);
-    window.addQuote = addQuote;
+  :  window.addQuote = addQuote;
    // Display a random quote initially
   displayRandomQuote();
 });
@@ -41,7 +40,43 @@ document.addEventListener('DOMContentLoaded', () => {
   
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    const newQuoteButton = document.getElementById('newQuote');
 
+    const quotes = [
+        { text: 'The greatest player of all time is Cristiano Ronaldo', category: 'Football' },
+        { text: 'Jesus is the way, the truth and the life.', category: 'Bible'},
+    ]
+    
+    function displayRandomQuote() {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        const quote = quotes[randomIndex];
+        console.log(`Displaying quote: "${quote.text}" - ${quote.category}`);
+        quoteDisplay.innerHTML = `"${quote.text}" - ${quote.category}`;
+      }
+    
+
+    function addQuote() {
+        const newQuoteText =  document.getElementById('newQuoteText').value.trim();
+        const newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
+
+    if (newQuoteText && newQuoteCategory) {
+        quotes.push({ text: newQuoteText, category: newQuoteCategory });
+        console.log('Added new quote: "${newQuoteText}" - ${newQuoteCategory}');
+        document.getElementById('newQuoteText').value = '';
+        document.getElementById('newQuoteCategory').value = '';
+        alert('New quote added successfully!');
+    } else {
+        console.log('Failed to add quote: Missing text or category');
+        alert('Please enter both a quote and a category.');
+      } }
+    
+    newQuoteButton.addEventListener('click', displayRandomQuote);
+  :  window.addQuote = addQuote;
+   // Display a random quote initially
+  displayRandomQuote();
+});
 
 
 
